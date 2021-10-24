42b0c02c79801115e5dff2a0735300ea92783599
Sinan Onalan <sonalan@happymoney.com>
2021-10-23 19:51:48 -0700

    delete image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -11,14 +10,7 @@ const IndexPage = () => (
     <h1>Neuro Heal</h1>
     <p>Welcome to your own world.</p>
     <p>Are you ready to meet real yourself?</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
